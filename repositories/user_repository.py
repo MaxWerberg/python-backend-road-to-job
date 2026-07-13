072579a1f7a5c9d7815cab65ffaa349a92d6a353
Maxim <euroset.sar50@gmail.com>
2026-07-14 00:39:55 +0400

    Обновил класс UserRepository

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -2,25 +2,40 @@ from models.user import User
 
 
 class UserRepository:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self):
+        self.db = []
 
     def create(self, user: User) -> User:
         """Создает юзера"""
-        return
+        self.db.append(user)
+        return user
 
     def get_by_id(self, user_id: int) -> User | None:
         """Поиск юзера по ID"""
-        return
+        for user in self.db:
+            if user.id == user_id:
+                return user
+        return None
 
     def get_by_email(self, email: str) -> User | None:
         """Поиск юзера по EMAIL"""
-        return
+        for user in self.db:
+            if user.email == email:
+                return user
+        return None
 
-    def update(self, user: User) -> User:
+    def update(self, updated_user: User) -> User | None:
         """Сохраняет изменения юзера в базу данных"""
-        return
+        for index, existing_user in enumerate(self.db):
+            if existing_user.id == updated_user.id:
+                self.db[index] = updated_user
+                return updated_user
+        return None
 
     def delete(self, user_id: int) -> bool:
         """Удаляет юзера по ID"""
-        return
+        for user in self.db:
+            if user.id == user_id:
+                self.db.remove(user)
+                return True
+        return False
