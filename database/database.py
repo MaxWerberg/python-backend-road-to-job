cf24ca37a2b6e4dfdd3bf2fae671a938c12e2c03
Maxim <euroset.sar50@gmail.com>
2026-07-16 17:47:00 +0400

    Подключил зависимости FastAPI для UserService

--- database/database.py
+++ database/database.py
@@ -1,5 +1,3 @@
-from contextlib import contextmanager
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
@@ -12,7 +10,6 @@ class Base(DeclarativeBase):
     pass
 
 
-@contextmanager
 def get_db():
     with SessionLocal() as db:
         try:
--- routers/user_router.py
+++ routers/user_router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from dependencies.service_dependencies import get_user_service as serv_dep
+from services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("/")
-def get_users():
-    return {"message": "эндпоинт юзеров"}
+def get_users(user_service: UserService = Depends(serv_dep)):
+    return {"message": "UserService ok"}
