cae1b4b0d625441cd2fb1beabae67b6dca397987
Maxim <euroset.sar50@gmail.com>
2026-09-06 20:48:19 +0400

    Добавил метод get_all_items_by_cart_id в CartItemRepository

--- repositories/cart_item_repository.py
+++ repositories/cart_item_repository.py
@@ -20,6 +20,12 @@ class CartItemRepository:
         )
         return self.db.execute(cart_query).scalar_one_or_none()
 
+    def get_all_items_by_cart_id(self, cart_id: int) -> list[CartItem]:
+        """Находит все товары корзины"""
+        query = select(CartItem).where(CartItem.cart_id == cart_id)
+
+        return self.db.execute(query).scalars().all()
+
     def update(self, cart_item: CartItem) -> CartItem:
         """Обновляет элемент в корзине"""
         self.db.flush()
